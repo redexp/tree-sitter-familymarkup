4314a1ed524ea1077e19b0dedb1416cb43ce3051
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2024-06-05 15:20:01 +0300

    added multi_newline

--- grammar.js
+++ grammar.js
@@ -5,21 +5,55 @@ module.exports = grammar({
 		$._nl_eof,
 	],
 
-  conflicts: $ => [[$.family]],
+  conflicts: $ => [
+    [$.family],
+    [$.relations],
+  ],
 
 	rules: {
-		root: $ => repeat($.family),
+		root: $ => repeatWith($.family, $._multi_newline),
+
+    _multi_newline: _ => /\r?\n\s*\r?\n/,
 
 		family: $ => seq(
-      $.name,
-      $._nl_eof,
-      repeat($.relation)
+      $.name_desc,
+      optional(seq(
+        $._multi_newline,
+        $.relations
+      ))
     ),
 
+    relations: $ => repeatWith($.relation, $._multi_newline),
+
     relation: $ => seq(
-      $.name, '=', $.name, $._nl_eof
+      $.name,
+      '=',
+      $.name_desc
+    ),
+
+    // source: $ => prec.left(repeatWith($.name, choice('+', ','))),
+    // target: $ => prec.left(repeatWith($.name_desc, ',')),
+
+    name_desc: $ => seq(
+      $.name,
+      optional(
+        $.name_aliases
+      )
+    ),
+
+    name_aliases: $ => seq(
+      '(', optional(repeatWith($.name, ',')), ')'
     ),
 
     name: _ => /\p{Lu}[\p{L}\-\d]*/u,
   },
 });
+
+function repeatWith(rule, sep) {
+  return seq(
+    rule,
+    optional(
+      seq(sep, repeat(rule))
+    )
+  );
+}
